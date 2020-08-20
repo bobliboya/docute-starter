2454642d706b5c68df1b6d77b030e268cd917034
Boyang Li <bobliboya@hotmail.com>
2020-08-21 00:28:12 +0800

    SAT_open

--- Wan_Wu/docs/about.md
+++ Wan_Wu/docs/about.md
@@ -1,5 +1,22 @@
-# About
+# 关于《万物大历史》在线版
 
-This is a simple doc website created with [Docute](https://docute.org)!
+&emsp;&emsp;本书著作权属于本书作者，本站仅负责转载。
 
-<a href="https://apps.apple.com/us/app/shadowrocket/id932747118" class="btn text-uppercase green-color branding-bg-color">[iOS]Potatso Lite · App Store▶</a>
+## 版权信息
+
+### 中文版
+
+**原作者**  [美]大卫· 克里斯蒂安 (David Christian) 
+
+**译者**  孙岳
+
+**出版社**  中信出版集团
+
+
+
+## Docute 开源内容
+
+&emsp;&emsp;本文档基于Docute开源文档管理系统创作而成。
+
+&emsp;&emsp;This is a simple doc website created with [Docute](https://docute.org)!
+
---
+++ sat/script.js
@@ -0,0 +1,50 @@
+/* globals Docute */
+
+new Docute({
+    layout: 'left',
+    target: '#docute',
+    sourcePath: './docs/',
+    nav: [{
+            title: 'Home',
+            link: '/README'
+        },
+        {
+            title: 'About',
+            link: '/about'
+        }
+    ],
+    /* 导航栏 */
+    sidebar: [{
+        /*第一组*/
+        title: '序言',
+        links: [{
+                title: '终身学习者一定会喜欢这部万物大历史',
+                link: '/xuyan/Gates'
+            },
+            {
+                title: '中文版序',
+                link: '/xuyan/installation'
+            },
+            {
+                title: '前言',
+                link: '/xuyan/qianyan'
+            },
+            {
+                title: 'Preface',
+                link: '/xuyan/preface'
+            },
+            {
+                title: '绪论',
+                link: '/xuyan/xulun'
+            },
+            {
+                title: 'Introduction',
+                link: '/xuyan/introduction'
+            },
+
+        ]
+
+
+
+    }],
+})
