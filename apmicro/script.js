d4d79ae4141a13df180b17ceb00b983c04c10af9
Boyang Li <bobliboya@hotmail.com>
2020-09-04 09:15:11 +0800

    Micro JSON update

--- apmicro/script.js
+++ apmicro/script.js
@@ -16,36 +16,21 @@ new Docute({
     /* 导航栏 */
     sidebar: [{
         /*第一组*/
-        /*
-                title: '序言',
-                links: [{
-                        title: 'Introduction',
-                        link: '/start/intro'
-                    },
-                    {
-                        title: '10 Basic Principles of Economy',
-                        link: '/start/10prin'
-                    },
-                    {
-                        title: '前言',
-                        link: '/start/qianyan'
-                    },
-                    {
-                        title: 'Preface',
-                        link: '/start/preface'
-                    },
-                    {
-                        title: '绪论',
-                        link: '/start/xulun'
-                    },
-                    {
-                        title: 'Introduction',
-                        link: '/start/introduction'
-                    },
 
-                ]
+        title: 'Introductions',
+        links: [{
+                title: 'Introduction',
+                link: '/start/intro'
+            },
+            {
+                title: '10 Principles of Economy',
+                link: '/start/10prin'
+            },
 
-        */
+
+        ]
+
+
 
     }],
 })
